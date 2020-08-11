9d519f18f86c3d6efa56b23f3caab77ba3ca4511
RockW <rock7699@gmail.com>
2020-08-11 15:29:59 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+const session = require("express-session");
+const cors = require("cors");
+const mongostore = require("connect-mongo")(session);
+
+/* port */
+
+const PORT = 3001 || process.env.PORT;
+
+/* Cors */
+
+const corsOptions = {
+	origin: ["http://localhost:3000"],
+	credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+/* bodyparser */
+
+app.use(bodyparser.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+	console.log("server running");
+});
