9f4563c3948f300150a6610bfd81da027f283cc7
RockW <rock7699@gmail.com>
2020-08-11 17:07:29 -0700

    adds auth

---
+++ controllers/auth.js
@@ -0,0 +1,40 @@
+const bcrypt = require("bcryptjs");
+
+const db = require("../models");
+
+const register = async (req, res) => {
+	try {
+		const foundUser = await db.Auth.findOne({ email: req.body.email });
+		if (foundUser) {
+			return res.status(400).json({
+				status: 400,
+				message: "Email Address Has Already Been Registered",
+			});
+		}
+		const salt = await bcrypt.genSalt(10);
+		const hash = await bcrypt.hash(req.body.password, salt);
+		const createdUser = await db.Auth.create({
+			email: req.body.email,
+			password: hash,
+		});
+		const createdProfile = await db.Profile.create({
+			username: req.body.username,
+		});
+		req.session.user = createdUser;
+		return res.status(201).json({
+			status: 201,
+			message: "Success",
+			createdUser: createdProfile,
+		});
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({
+			status: 500,
+			message: "Something went wrong please try again.",
+		});
+	}
+};
+
+module.exports = {
+	register,
+};
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyparser = require("body-parser");
 const session = require("express-session");
 const cors = require("cors");
 const mongostore = require("connect-mongo")(session);
+const routes = require("./routes");
 
 /* port */
 
@@ -23,6 +24,20 @@ app.use(cors(corsOptions));
 app.use(bodyparser.urlencoded({ extended: false }));
 
 app.use(express.json());
+
+app.use(
+	session({
+		store: new mongostore({
+			url: "mongodb://localhost:27017/react-store",
+		}),
+		secret: "Secret",
+		resave: false,
+		saveUninitialized: false,
+		cookie: { maxAge: 1000 * 60 * 60 * 20 * 7 * 2 },
+	})
+);
+
+app.use("/api/v1/auth", routes.auth);
 
 app.listen(PORT, () => {
 	console.log(`server running on http://localhost:${PORT}`);
